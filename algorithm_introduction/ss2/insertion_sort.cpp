d376f3e6264668c1d8d707816b25b827aac44e5d
1izard <dee1bdnatiaw@gmail.com>
2019-12-12 23:29:34 +0900

    Add insertion_sort.cpp

--- algorithm_introduction/ss2/insertion_sort.cpp
+++ algorithm_introduction/ss2/insertion_sort.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int main() {
-  cout << "Hello" << endl;
-  vector<int> vec(0);
-  for (int i = 0; i < 10; ++i) {
-    vec.push_back(i);
+void display_array(const int arr[], size_t size) {
+  for (int i = 0; i < size; ++i) {
+    cout << arr[i] << ", ";
   }
+  cout << endl;
+}
+
+void insertion_sort(int arr[], size_t size) {
+  if (size <= 1) return;
+  for (int i = 1; i < size; ++i) {
+    int key = arr[i];
+    int j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      --j;
+    }
+    arr[j + 1] = key;
+  }
+}
+
+int main() {
+  int A[] = {3, 1, 5, 9, 4};
+
+  cout << "before: ";
+  display_array(A, end(A) - begin(A));
+
+  insertion_sort(A, end(A) - begin(A));
+
+  cout << "after: ";
+  display_array(A, end(A) - begin(A));
 }
