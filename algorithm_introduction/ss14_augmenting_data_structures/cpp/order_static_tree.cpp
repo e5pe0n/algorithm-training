176f9b5ebcfc6fe0ccc1b6e42767bb12b15b0874
1izard <dee1bdnatiaw@gmail.com>
2020-04-28 16:14:58 +0900

    Add order static trees

---
+++ algorithm_introduction/ss14_augmenting_data_structures/cpp/order_static_tree.cpp
@@ -0,0 +1,260 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+enum class Color { BLACK, RED };
+struct Node {
+  int key;
+  int size = 0;
+  Node *left = nullptr;
+  Node *right = nullptr;
+  Node *p = nullptr;
+  Color color = Color::RED; // black: 1, red: 1
+  Node(int key, Node *p, Color color) : key(key), p(p), color(color) {}
+};
+
+struct Tree {
+
+  Node *root;
+  Node nil = Node(0, nullptr, Color::BLACK);
+
+  Tree(Node *root) : root(root) {}
+
+  Node *tree_minimum(Node *x) {
+    while (x->left != &nil)
+      x = x->left;
+    return x;
+  }
+
+  void left_rotate(Node *x) {
+    Node *y = x->right;
+
+    x->right = y->left;
+    if (y->left != &nil)
+      y->left->p = x;
+    y->p = x->p;
+
+    if (x->p == &nil)
+      root = y;
+    else if (x == x->p->left)
+      x->p->left = y;
+    else
+      x->p->right = y;
+
+    y->left = x;
+    x->p = y;
+    y->size = x->size;
+    x->size = x->left->size + x->right->size + 1;
+  }
+
+  void right_rotate(Node *x) {
+    Node *y = x->left;
+
+    x->left = y->right;
+    if (y->right != &nil)
+      y->right->p = x;
+    y->p = x->p;
+
+    if (x->p == &nil)
+      root = y;
+    else if (x->p->left)
+      x->p->left = y;
+    else
+      x->p->right = y;
+
+    y->right = x;
+    x->p = y;
+    y->size = x->size;
+    x->size = x->left->size + x->right->size + 1;
+  }
+
+  void _os_insert_fixup(Node *z) {
+    Node *y = nullptr;
+    while (z->p->color == Color::RED) {
+      if (z->p == z->p->p->left) {
+        y = z->p->p->left;
+        if (y->color == Color::RED) {
+          z->p->color = Color::BLACK;
+          y->color = Color::BLACK;
+          z->p->p->color = Color::RED;
+          z = z->p->p;
+        } else {
+          if (z == z->p->right) {
+            z = z->p;
+            left_rotate(z);
+          }
+          z->p->color = Color::BLACK;
+          z->p->p->color = Color::RED;
+          right_rotate(z->p->p);
+        }
+      } else {
+        y = z->p->p->right;
+        if (y->color == Color::RED) {
+          z->p->color = Color::BLACK;
+          y->color = Color::BLACK;
+          z->p->p->color = Color::RED;
+          z = z->p->p;
+        } else {
+          if (z == z->p->left) {
+            z = z->p;
+            right_rotate(z);
+          }
+          z->p->color = Color::BLACK;
+          z->p->p->color = Color::RED;
+          left_rotate(z->p->p);
+        }
+      }
+      root->color = Color::BLACK;
+    }
+  }
+
+  void os_insert(Node *z) {
+    Node *y = &nil;
+    Node *x = root;
+    while (x != &nil) {
+      y = x;
+      y->size++;
+      if (z->key < x->key)
+        x = x->left;
+      else
+        x = x->right;
+    }
+    z->p = y;
+    if (y == &nil)
+      root = z;
+    else if (z->key < y->key)
+      y->left = z;
+    else
+      y->right = z;
+
+    z->left = &nil;
+    z->right = &nil;
+    z->color = Color::RED;
+    _os_insert_fixup(z);
+  }
+
+  void _os_transplant(Node *u, Node *v) {
+    if (u->p == &nil)
+      root = v;
+    else if (u == u->p->left)
+      u->p->left = v;
+    else
+      u->p->right = v;
+    u->p = v->p;
+  }
+
+  void _os_delete_fixup(Node *x) {
+    Node *w = nullptr;
+    while (x != root && x->color == Color::BLACK) {
+      if (x == x->p->left) {
+        w = x->p->right;
+        if (w->color == Color::RED) {
+          w->color = Color::BLACK;
+          x->p->color = Color::RED;
+          left_rotate(x->p);
+          w = x->p->right;
+        }
+        if (w->left->color == Color::BLACK && w->right->color == Color::BLACK) {
+          w->color = Color::RED;
+          x = x->p;
+        } else {
+          if (w->right->color == Color::BLACK) {
+            w->left->color = Color::BLACK;
+            w->color = Color::RED;
+            right_rotate(w);
+            w = x->p->right;
+          }
+          w->color = x->p->color;
+          x->p->color = Color::BLACK;
+          w->right->color = Color::BLACK;
+          left_rotate(x->p);
+          x = root;
+        }
+      } else {
+        w = x->p->left;
+        if (w->color == Color::RED) {
+          w->color = Color::BLACK;
+          x->p->color = Color::RED;
+          right_rotate(x->p);
+          w = x->p->left;
+        }
+        if (w->left->color == Color::BLACK && w->right->color == Color::BLACK) {
+          w->color = Color::RED;
+          x = x->p;
+        } else {
+          if (w->left->color == Color::BLACK) {
+            w->right->color = Color::BLACK;
+            w->color = Color::RED;
+            left_rotate(w);
+            w = x->p->left;
+          }
+          w->color = x->p->color;
+          x->p->color = Color::BLACK;
+          w->left->color = Color::BLACK;
+          right_rotate(x->p);
+          x = root;
+        }
+      }
+    }
+    x->color = Color::BLACK;
+  }
+
+  void os_delete(Node *z) {
+    Node *y = z;
+    Node *x = nullptr;
+    Color y_original_color = y->color;
+    if (z->left == &nil) {
+      x = z->right;
+      _os_transplant(z, z->right);
+    } else if (z->right == &nil) {
+      x = z->left;
+      _os_transplant(z, z->left);
+    } else {
+      y = tree_minimum(z->right);
+      y_original_color = y->color;
+      x = y->right;
+      if (y->p == z)
+        x->p = y;
+      else {
+        _os_transplant(y, y->right);
+        y->right = z->right;
+        y->right->p = y;
+      }
+      y->left = z->left;
+      y->left->p = y;
+      y->color = z->color;
+    }
+    if (y_original_color == Color::BLACK)
+      _os_delete_fixup(x);
+  }
+
+  Node *os_select(Node *x, int i) {
+    int r = x->left->size + 1;
+    if (i == r)
+      return x;
+    else if (i < r)
+      os_select(x->left, i);
+    else
+      os_select(x->right, i - r);
+  }
+
+  int os_rank(Node *x) {
+    int r = x->left->size + 1;
+    while (x != root) {
+      if (x == x->p->right)
+        r += x->p->left->size + 1;
+      x = x->p;
+    }
+    return r;
+  }
+};
+
+string node_str(Node *x) {
+  if (x == nullptr)
+    return "null";
+  else
+    return to_string(x->key);
+}
+
+int main() {}
---
+++ algorithm_introduction/ss14_augmenting_data_structures/python/order_static_tree.py
@@ -0,0 +1,233 @@
+from enum import Enum
+
+
+class Color(Enum):
+    BLACK = 0
+    RED = 1
+
+
+class Node:
+    def __init__(
+        self,
+        key: int,
+        color: Color,
+        left=None,   # Node
+        right=None,  # Node
+        p=None,      # Node
+    ):
+        self.key = key
+        self.color = color
+        self.left = left
+        self.right = right
+        self.p = p
+
+    def __str__(self):
+        l_key = self.left.key if self.left else None
+        r_key = self.right.key if self.right else None
+        return 'key: %s, color: %s, left: %s, right: %s' % (self.key, self.color.name, l_key, r_key)
+
+
+class Tree:
+    def __init__(
+        self,
+        root    # Node
+    ):
+        self.root = root
+        self.nil = None
+
+    def tree_minimum(self, x: Node):
+        while x.left is not self.nil:
+            x = x.left
+        return x
+
+    def _os_transplant(self, u: Node, v: Node):
+        if u is self.root:
+            self.root = v
+        elif u.p.left is u:
+            u.p.left = v
+        else:
+            u.p.right = v
+        v.p = u.p
+
+    def left_rotate(self, x: Node):
+        y = x.right
+
+        x.right = y.left
+        if y.left != self.nil:
+            y.left.p = x
+        y.p = x.p
+
+        if x.p == self.nil:
+            self.root = y
+        elif x.p.left == x:
+            x.p.left = y
+        else:
+            x.p.right = y
+
+        y.left = x
+        x.p = y
+        y.size = x.size
+        x.size = x.left.size + x.right.size + 1
+
+    def right_rotate(self, x: Node):
+        y = x.left
+
+        x.left = y.right
+        if y.right != self.nil:
+            y.right.p = x
+        y.p = x.p
+
+        if x.p == self.nil:
+            self.root = y
+        elif x.p.left == x:
+            x.p.left = y
+        else:
+            x.p.right = y
+
+        y.right = x
+        x.p = y
+        y.size = x.size
+        x.size = x.left.size + x.right.size + 1
+
+    def _os_insert_fixup(self, z: Node):
+        while z.p.color is Color.RED:
+            if z.p is z.p.p.left:
+                y = z.p.p.left
+                if y.color is Color.RED:
+                    z.p.color = Color.BLACK
+                    y.color = Color.BLACK
+                    z.p.p.color = Color.RED
+                    z = z.p.p
+                else:
+                    if z is z.p.right:
+                        z = z.p
+                        self.left_rotate(z)
+                    z.p.color = Color.BLACK
+                    z.p.p.color = Color.RED
+                    self.right_rotate(z)
+            else:
+                y = z.p.p.right
+                if y.color is Color.RED:
+                    z.p.color = Color.BLACK
+                    y.color = Color.BLACK
+                    z.p.p.color = Color.RED
+                    z = z.p.p
+                else:
+                    if z is z.p.left:
+                        z = z.p
+                        self.right_rotate(z)
+                    z.p.color = Color.BLACK
+                    z.p.p.color = Color.RED
+                    self.left_rotate(z.p.p)
+            self.root.color = Color.BLACK
+
+    def os_insert(self, z: Node):
+        y = self.nil
+        x = self.root
+        while x is not self.nil:
+            y = x
+            y.size += 1
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        z.p = y
+        if y is self.nil:
+            self.root = z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+        z.left = self.nil
+        z.right = self.nil
+        z.color = Color.RED
+        self._os_insert_fixup(z)
+
+    def _os_delete_fixup(self, x: Node):
+        while x is not self.root and x.color is Color.BLACK:
+            if x is x.p.left:
+                w = x.p.right
+                if w.color is Color.RED:
+                    w.color = Color.BLACK
+                    x.p.color = Color.RED
+                    self.left_rotate(x.p)
+                    w = x.p.right
+                if w.left.color is Color.BLACK and w.right.color is Color.BLACK:
+                    w.color = Color.RED
+                    x = x.p
+                else:
+                    if w.right.color is Color.BLACK:
+                        w.left.color = Color.BLACK
+                        w.color = Color.RED
+                        self.right_rotate(w)
+                        w = x.p.right
+                    w.color = x.p.color
+                    x.p.color = Color.BLACK
+                    x.right.color = Color.BLACK
+                    self.left_rotate(x.p)
+                    x = self.root
+            else:
+                w = x.p.left
+                if w.color is Color.RED:
+                    w.color = Color.BLACK
+                    x.p.color = Color.RED
+                    self.right_rotate(x.p)
+                    w = w.p.left
+                if w.left.color is Color.BLACK and w.right.color is Color.BLACK:
+                    w.color = Color.RED
+                    x = x.p
+                else:
+                    if w.left.color is Color.BLACK:
+                        w.right.color = Color.BLACK
+                        w.color = Color.RED
+                        self.left_rotate(w)
+                        w = x.p.left
+                    w.color = x.p.color
+                    x.p.color = Color.BLACK
+                    x.left.color = Color.BLACK
+                    self.right_rotate(x.p)
+                    x = self.root
+        x.color = Color.BLACK
+
+    def os_delete(self, z: Node):
+        y = z
+        y_original_color = y.color
+        if z.left is self.nil:
+            x = z.right
+            self._os_transplant(z, z.right)
+        elif z.right is self.nil:
+            x = z.left
+            self._os_transplant(z, z.left)
+        else:
+            y = self.tree_minimum(z.right)
+            y_original_color = y.color
+            x = y.right
+            if y.p == z:
+                x.p = y
+            else:
+                self._os_transplant(y, y.right)
+                y.right = z.right
+                y.right.p = y
+            self._os_transplant(z, y)
+            y.left = z.left
+            y.left.p = y
+            y.color = z.color
+        if y_original_color is Color.BLACK:
+            self._os_delete_fixup(x)
+
+    def os_select(self, x: Node, i: int) -> Node:
+        r = x.left.size + 1
+        if i == r:
+            return x
+        elif i < r:
+            return self.os_select(x.left, i)
+        else:
+            return self.os_select(x.right, r - i)
+
+    def os_rank(self, x: Node) -> int:
+        r = x.left.size + 1
+        while x is not self.root:
+            if x is x.p.right:
+                r += x.p.left.size + 1
+            x = x.p
+        return r
