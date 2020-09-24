56bc76d206a8f0d35cedc7341a620ebb8304c0c4
1izard <1izard.synchr0fi5h@gmail.com>
2020-09-24 04:02:05 +0000

    update run.sh

--- template/cpp/run.sh
+++ template/cpp/run.sh
@@ -2,5 +2,18 @@
 
 f=`echo $1 | sed -e 's/\(.*\).cpp/\1/'`
 current_dir=$(eval pwd)
-clang++ -std=c++14 -g -o ${current_dir}/build/${f}.out $1
-eval ${current_dir}/build/${f}.out
+
+c=`which clang++`
+if [ -x $c ] && [ ! -z $c ]
+then
+    clang++ -std=c++14 -g -o ${current_dir}/build/${f}.out $1
+    eval ${current_dir}/build/${f}.out
+    exit
+fi
+
+c=`which g++`
+if [ -x $c ] && [ ! -z $c ]
+then
+    g++ -std=c++14 -g -o ${current_dir}/build/${f}.out $1
+    eval ${current_dir}/build/${f}.out
+fi
